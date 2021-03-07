876f8ad1adc039f93a2f47a0999d273bcc1ba1a4
CoyTorreblanca <coydiego@knights.ucf.edu>
2021-03-07 17:09:10 -0600

    fix lack of bit strings

--- lib/inv.py
+++ lib/inv.py
@@ -3,7 +3,7 @@ from cc import turtle
 def select_by_name(name):
 	for i in range(16):
 		item = turtle.getItemDetail(i + 1)
-		if item is not None and item['name'] == name:
+		if item is not None and item[b'name'] == name:
 			turtle.select(i + 1)
 			return True
 	return False
@@ -12,7 +12,7 @@ def select_by_name(name):
 def select_from_dict(dict):
 	for i in range(16):
 		item = turtle.getItemDetail(i + 1)
-		if item is not None and item['name'] in dict:
+		if item is not None and item[b'name'] in dict:
 			turtle.select(i + 1)
 			return True
 	return False
@@ -31,7 +31,7 @@ def drop_all_except(keep):
 	initial_slot = turtle.getSelectedSlot()
 	for i in range(16):
 		item = turtle.getItemDetail(i + 1)
-		if item is not None and item['name'] not in keep:
+		if item is not None and item[b'name'] not in keep:
 			# found item not in keep
 			turtle.select(i + 1)
 			turtle.drop()
@@ -52,24 +52,24 @@ def restack():
 	for i in range(16):
 		item = turtle.getItemDetail(i + 1)
 		if item is not None:
-			if item['name'] in inv:
+			if item[b'name'] in inv:
 				# stack with space already in inventory
 				turtle.select(i + 1)
-				turtle.transferTo(inv[item['name']]['slot'])
-				prev_space = turtle.getItemSpace(inv[item['name']]['slot'])
+				turtle.transferTo(inv[item[b'name']]['slot'])
+				prev_space = turtle.getItemSpace(inv[item[b'name']]['slot'])
 				if prev_space > 0:
 					# previous stack still has space remaining
-					inv[item['name']]['space'] = prev_space
+					inv[item[b'name']]['space'] = prev_space
 				
 				elif turtle.getItemCount(i + 1) > 0:
 					# previous stack full, selected not empty
-					inv[item['name']] = {'space':turtle.getItemSpace(i + 1), 'slot':i + 1}
+					inv[item[b'name']] = {'space':turtle.getItemSpace(i + 1), 'slot':i + 1}
 				else:
 					# previous stack full, selected not empty
-					inv[item['name']] = null
+					inv[item[b'name']] = null
 			elif turtle.getItemSpace(i + 1) > 0:
 				# add item to inv if not there and still has space
-				inv[item['name']] = {'space':turtle.getItemSpace(i + i), 'slot':i + 1}
+				inv[item[b'name']] = {'space':turtle.getItemSpace(i + i), 'slot':i + 1}
 	turtle.select(initial_slot)
 
 def inv_dict():
