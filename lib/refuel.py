910722c090cf3258036e0e533ca3c48cec3135b8
CoyTorreblanca <coydiego@knights.ucf.edu>
2021-03-06 20:07:50 -0600

    refuel.py
    refuel fixes
    
    style changes

---
+++ lib/refuel.py
@@ -0,0 +1,22 @@
+from cc import import_file, turtle
+inv = import_file('/lib/inv.py')
+
+# TODO: add blaze rods, bamboo, kelp, etc.
+FUEL = {
+	'minecraft:coal': 80,
+	'minecraft:charcoal': 80,
+	'minecraft:oak_planks': 15,
+}
+
+# refuel a turtle if necessary
+# returns false if out of fuel
+def refuel(threshold=80):
+	initial_slot = turtle.getSelectedSlot()
+	while turtle.getFuelLevel() < threshold:
+		# select fuel
+		if not inv.select_from_dict(FUEL):
+			return false
+		turtle.refuel(1)
+
+	turtle.select(initial_slot)
+	return true
