cd8ee9cc9310c8478f3279a5ac9bc44a985cee75
CoyTorreblanca <coydiego@knights.ucf.edu>
2021-03-07 10:45:01 -0600

    fix /lib/fuel.py

--- lib/fuel.py
+++ lib/fuel.py
@@ -10,13 +10,13 @@ FUEL = {
 
 # refuel a turtle if necessary
 # returns false if out of fuel
-def refuel(threshold=80):
+def fuel(threshold=80):
 	initial_slot = turtle.getSelectedSlot()
 	while turtle.getFuelLevel() < threshold:
 		# select fuel
 		if not inv.select_from_dict(FUEL):
-			return false
+			return False
 		turtle.refuel(1)
 
 	turtle.select(initial_slot)
-	return true
+	return True
