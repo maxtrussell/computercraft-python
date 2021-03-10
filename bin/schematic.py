1b55324675129700059b5c24acae924bcdd708d6
CoyTorreblanca <coydiego@knights.ucf.edu>
2021-03-10 10:13:26 -0600

    fix

--- bin/schematic.py
+++ bin/schematic.py
@@ -58,10 +58,10 @@ def build_slice(slice, block_defs):
     
     for i in range(len(slice)):
         place()
-        navigator.force_dir(nav.DIRS.FORWARD, len(slice[i]) - 1, place())
+        navigator.force_dir(nav.DIRS.FORWARD, len(slice[i]) - 1, place)
 
         if i != len(slice)-1:
-            go_to(navigator.location[0] + 1, navigator.location[1], navigator.location[2])
+            navigator.go_to(navigator.location[0] + 1, navigator.location[1], navigator.location[2])
 
     # reset for the next level
     nav.force_dir(nav.DIRS.UP)
