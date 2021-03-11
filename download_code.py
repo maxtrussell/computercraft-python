47918919758363408e3ed3c9b7806a8bdf83d673
Max Trussell <maxtrussell@gmail.com>
2021-03-10 21:04:18 -0800

    parallelize download code

--- download_code.py
+++ download_code.py
@@ -1,32 +1,29 @@
 import requests
 
-from cc import fs
+from cc import fs, parallel
 
-BASE_URL = 'https://raw.githubusercontent.com/maxtrussell/computercraft-python/{}'
+BASE_URL = 'https://raw.githubusercontent.com/maxtrussell/computercraft-python/{}{}'
 FILES = [
     "/bin/curl.py",
     "/bin/file_server.py",
     "/bin/quarry.py",
     "/bin/schematic.py",
-    "/docs/cords.txt",
     "/lib/dig.py",
     "/lib/fuel.py",
     "/lib/inv.py",
     "/lib/nav.py",
     "/lib/net.py",
     "/schematics/house.txt",
-    "/schematics/farm.txt",
 ]
 
-branch = 'master'
-if len(args) == 1:
-    branch = args[0]
+def wrapper(func, f, branch):
+    def wrap():
+        return func(f, branch)
+    return wrap
 
-def get_file(filename):
-    resp = requests.get(BASE_URL.format(branch) + filename)
+def get_file(filename, branch):
+    resp = requests.get(BASE_URL.format(branch, filename))
     resp.raise_for_status()
-
-    print(f'Getting {filename}...')
     with fs.open(filename, 'w') as f:
         f.write(resp.text)
 
@@ -34,5 +31,8 @@ for d in ['/lib', '/bin', '/docs', '/schematics', '/scripts']:
     if not fs.exists(d):
         fs.makeDir(d)
 
-for f in FILES:
-    get_file(f)
+branch = args[0] if len(args) >= 1 else 'master'
+print(f'Getting files asynchronously:')
+for f in sorted(FILES):
+    print(f)
+parallel.waitForAll(*[wrapper(get_file, f, branch) for f in FILES])
