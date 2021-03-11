6a9b2825048702184b22a6a7e11e39cd1aebbda0
CoyTorreblanca <coydiego@knights.ucf.edu>
2021-03-10 18:56:56 -0600

    untabify

--- lib/dig.py
+++ lib/dig.py
@@ -69,7 +69,7 @@ def manage_inv(valuables, chest):
                 inv.restack()
 
         if chest and inv.is_full():
-		navigator = chest[2]
+                navigator = chest[2]
                 current_location = navigator.get_location()
                 current_direction = navigator.get_direction()
                 chest_location = chest[0]
